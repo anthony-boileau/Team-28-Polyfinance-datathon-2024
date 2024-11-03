146fad6baa3317b634c760d5674c49f6fcfef0db
ab <anthonyboileau@gmail.com>
2024-11-03 12:32:46 -0500

    ITS ALIIIIIIVE

---
+++ src/retriever.py
@@ -0,0 +1,12 @@
+def get_context(query):
+
+    data = '' #get data from db
+
+    context = """
+    Act as a financial analyst who quickly reviews annual reports,
+      summarizes key insights, and answers specific questions about
+        financial metrics and company performance 
+        based on the following text: \n{data}
+    
+    \nAnswer the following prompt:
+"""
--- src/transformer.py
+++ src/transformer.py
@@ -1,15 +1,64 @@
-import awscli as aws
-import boto3 as b
-import json
 
-def lambda_handler(event, context):
-    # Assume the input is a JSON object
-    input_data = event['data']
-    
-    # Transform the data (e.g., convert to uppercase)
-    transformed_data = input_data.upper()
-    
-    return {
-        'statusCode': 200,
-        'body': json.dumps({'transformed_data': transformed_data})
+import os
+import boto3
+import json
+from botocore.exceptions import ClientError
+from dotenv import load_dotenv
+
+# Load environment variables from the .secrets file
+load_dotenv('.secrets')
+AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_SESSION_TOKEN = os.getenv("AWS_SESSION_TOKEN")
+
+def main():
+
+    # Create a Bedrock Runtime client in the AWS Region of your choice.
+    client = boto3.client(
+        'bedrock-runtime',
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        aws_session_token=AWS_SESSION_TOKEN,
+        region_name=AWS_DEFAULT_REGION
+    )
+
+    # Set the model ID to Claude 3 Sonnet v1.
+    model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
+
+    # Define the prompt for the model.
+    prompt = "What is your purpose"
+
+    # Format the request payload using the model's native structure.
+    native_request = {
+        "anthropic_version": "bedrock-2023-05-31",
+        "max_tokens": 512,
+        "temperature": 0.5,
+        "messages": [
+            {
+                "role": "user",
+                "content": [{"type": "text", "text": prompt}],
+            }
+        ],
     }
+
+    # Convert the native request to JSON.
+    request = json.dumps(native_request)
+
+    try:
+        # Invoke the model with the request.
+        response = client.invoke_model(modelId=model_id, body=request)
+
+    except (ClientError, Exception) as e:
+        print(f"ERROR: Can't invoke '{model_id}'. Reason: {e}")
+        exit(1)
+
+    # Decode the response body.
+    model_response = json.loads(response["body"].read())
+
+    # Extract and print the response text.
+    response_text = model_response["content"][0]["text"]
+    print(response_text)
+
+if __name__ == "__main__":
+    main()
